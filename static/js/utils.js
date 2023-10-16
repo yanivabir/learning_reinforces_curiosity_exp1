ad2c4a437d2e5ead85d42b77d16cd6f7cf0621fd
yanivabir <yanivabir@gmail.com>
2023-10-16 11:41:41 +0300

    add missing objectToCSV function

--- static/js/utils.js
+++ static/js/utils.js
@@ -120,6 +120,40 @@ function saveData(PID, sess, part, data, onComplete = function() {}, type = 'csv
   }));
 }
 
+// Convert array of objects to csv string
+const objectToCsv = function (data) {
+     
+  const csvRows = [];
+
+  /* Get headers as every csv data format
+  has header (head means column name)
+  so objects key is nothing but column name
+  for csv data using Object.key() function.
+  We fetch key of object as column name for
+  csv */
+  const headers = Object.keys(data[0]);
+
+  /* Using push() method we push fetched
+     data into csvRows[] array */
+  csvRows.push(headers.join(','));
+
+  // Loop to get value of each objects key
+  for (const row of data) {
+      const values = headers.map(header => {
+          const val = row[header]
+          return `"${val}"`;
+      });
+
+      // To add, separator between each value
+      csvRows.push(values.join(','));
+  }
+
+  /* To add new line for each objects values
+     and this return statement array csvRows
+     to this function.*/
+  return csvRows.join('\n');
+};
+
 
 function createSecondSesssList(keysToRemove) {
   questions = jsPsych.data.get().filter({category: "wait_question",
